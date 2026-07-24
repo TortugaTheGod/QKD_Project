18f0fcc622c8f987c9b2d8acd366fd6a2048d758
Avyay Kodali <114587414+Avyay123@users.noreply.github.com>
2026-07-24 10:24:37 -0700

    Early implementation of QCL

---
+++ QCL.py
@@ -0,0 +1,94 @@
+my_protocol = BB84(eve_intercept='yes')
+
+
+
+my_qubits = cirq.NamedQubit.range(3, prefix='q')
+
+signal = my_qubits[0]
+eve = my_qubits[1]
+ancilla = my_qubits[2]
+my_protocol.qubit = signal
+my_protocol.simulator = cirq.Simulator()
+
+
+
+# alice Circuit
+circuit = cirq.Circuit()
+
+circuit.append(cirq.I(signal))
+
+my_protocol.alice_send_0_no_H_circuit = circuit
+
+
+
+circuit = cirq.Circuit()
+
+circuit.append(cirq.I(signal))
+circuit.append(cirq.H(signal))
+
+my_protocol.alice_send_0_H_circuit = circuit
+
+
+# bob circ
+circuit = cirq.Circuit()
+
+
+circuit.append(cirq.I(signal))
+circuit.append(cirq.measure(signal))
+
+my_protocol.bob_receive_no_H_circuit = circuit
+
+
+sim = cirq.Simulator()
+
+circuit = cirq.Circuit()
+
+
+import numpy as np
+
+
+theta = 0
+
+
+def build_pccm(theta, alice_used_H=False):
+  pccm = cirq.Circuit()
+
+  pccm.append(cirq.rx(np.pi/2)(signal))
+  pccm.append(cirq.ry(theta)(eve).controlled_by(signal))
+  pccm.append(cirq.ry(-(np.pi))(signal).controlled_by(eve))
+  pccm.append(cirq.rx(-(np.pi/2))(signal))
+  pccm.append(cirq.rx(-(np.pi/2))(eve))
+  return pccm
+
+ # if bob and alice use H, append H to eve's bit in pccm
+
+def loss_function(theta):
+    pccm = build_pccm(theta, alice_used_H = True)
+
+    circuit = cirq.Circuit()
+
+    circuit += my_protocol.alice_send_0_H_circuit
+    circuit += pccm
+
+    result = sim.simulate(circuit)
+
+    fidelity = ???????
+    return fidelity
+
+
+if my_protocol.does_alice_apply_H == "yes" and my_protocol.does_bob_apply_H == "yes":
+    pccm.append(cirq.H(eve))
+
+circuit = cirq.Circuit()
+
+fid = cirq.Circuit()
+
+fid += my_protocol.alice_send_0_H_circuit
+fid += pccm
+
+
+
+result = sim.simulate(fid)
+state = result.final_state_vector
+state=np.abs(state)**2
+state
